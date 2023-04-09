49a07853242676cc62816cecea7df1d0c14c6960
stonega <xijieyin@gmail.com>
2023-04-09 14:48:43 +0800

    feat: add cache

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, send_file, request, jsonify
+from flask import Flask, Response, send_file, request, jsonify
 from pydub import AudioSegment
+from tqdm import tqdm
 import io
 import requests
+import zipfile
+from flask_caching import Cache
 
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "MAX_CONTENT_LENGTH": 100 * 1024 * 1024  # 100 MB max upload limit
+}
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 100 MB max upload limit
+app.config.from_mapping(config)
+cache = Cache(app)
 
 @app.route('/')
 def hello_world():
@@ -23,54 +33,55 @@ def slice_audio(audio, slice_duration):
 
     return slices
 
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        audio = AudioSegment.from_file(file)
-        sliced_audios = slice_audio(audio, 10 * 60 * 1000)  # Slice into max 10-minute chunks
-        
-        # Combine the sliced audio files into a single response
-        result_bytes = io.BytesIO()
-        for idx, audio in enumerate(sliced_audios):
-            slice_io = io.BytesIO()
-            audio.export(slice_io, format="mp3")
-            slice_io.seek(0)
-
-            if idx == 0:
-                result_bytes.write(b'--sliceseparator\r\n')
-            result_bytes.write(slice_io.read())
-            result_bytes.write(b'\r\n--sliceseparator\r\n')
-
-        result_bytes.seek(0)
-
-        return send_file(result_bytes, mimetype='audio/mpeg', as_attachment=True, attachment_filename='sliced_audio.mp3')
-    
-@app.route('/fetch', methods=['GET'])
-def fetch_and_slice_audio():
-    if request.method == 'GET':
-        audio_url = request.query['url']
-
-        response = requests.get(audio_url)
-        if response.status_code == 200:
-            audio = AudioSegment.from_file(io.BytesIO(response.content))
-            sliced_audios = slice_audio(audio, 10 * 60 * 1000)  # Slice into max 10-minute chunks
-
-            # Combine the sliced audio files into a single response
-            result_bytes = io.BytesIO()
+def zip_audios(sliced_audios):
+    # Combine the sliced audio files into a single response
+    with io.BytesIO() as zip_buffer:
+        with zipfile.ZipFile(zip_buffer, 'w', compression=zipfile.ZIP_DEFLATED) as zipf:
             for idx, audio in enumerate(sliced_audios):
                 slice_io = io.BytesIO()
                 audio.export(slice_io, format="mp3")
                 slice_io.seek(0)
+                zipf.writestr(f'slice_{idx+1}.mp3', slice_io.read())
+            zip_buffer.seek(0)
+            return zip_buffer.read()
 
-                if idx == 0:
-                    result_bytes.write(b'--sliceseparator\r\n')
-                result_bytes.write(slice_io.read())
-                result_bytes.write(b'\r\n--sliceseparator\r\n')
 
-            result_bytes.seek(0)
+@app.route('/upload', methods=['POST'])
+@cache.cached(timeout=50)
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        audio = AudioSegment.from_file(file)
+        sliced_audios = slice_audio(audio, 20 * 60 * 1000)  # Slice into max 10-minute chunks
+        zip = zip_audios(sliced_audios)
+        return Response(zip, headers={
+            'Content-Disposition': 'attachment; filename=audio.zip',
+            'Content-Type': 'application/zip'
+        })
+        
+    
+@app.route('/slice', methods=['GET'])
+@cache.cached(timeout=50)
+def fetch_and_slice_audio():
+    if request.method == 'GET':
+        audio_url = request.args['url']
+        print(audio_url)
+        response = requests.get(audio_url, stream=True)
+        total_size = int(response.headers.get('content-length', 0))
 
-            return send_file(result_bytes, mimetype='audio/mpeg', as_attachment=True, attachment_filename='sliced_audio.mp3')
+        # Initialize the bytearray
+        content = bytearray()
+
+        for data in tqdm(response.iter_content(chunk_size = 1024 * 1024), total = total_size // (1024 * 1024) , unit = 'MB', unit_scale = True):
+            content.extend(data)
+
+        if response.status_code == 200:
+            audio = AudioSegment.from_file(io.BytesIO(content))
+            sliced_audios = slice_audio(audio, 20 * 60 * 1000)  # Slice into max 20-minute chunks
+            zip = zip_audios(sliced_audios)
+            return Response(zip, headers={
+                'Content-Disposition': 'attachment; filename=audio.zip',
+                'Content-Type': 'application/zip'
+            })
         else:
             return jsonify({'error': 'Failed to fetch the audio URL'}), 404
