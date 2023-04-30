cab9102be02ed92cae567141372c4a5cdf95e0f0
stonega <xijieyin@gmail.com>
2023-04-30 17:54:48 +0800

    feat: support transcript

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 from datetime import datetime
-from typing import Annotated
-from fastapi import FastAPI, Form, File, HTTPException, Response, UploadFile
+import os
+from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.responses import StreamingResponse
 from pydub import AudioSegment
 from tqdm import tqdm
 import io
 import requests
 import zipfile
+import openai
+import uuid
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 
-config = {
-    "DEBUG": True,          # some Flask specific configs
-    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 300,
-    "MAX_CONTENT_LENGTH": 100 * 1024 * 1024  # 100 MB max upload limit
-}
+load_dotenv()
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "_")
 
 ALLOWED_EXTENSIONS = {'mp3', 'mp4', 'mpeg', 'mpga', 'm4a', 'wav', 'webm'}
 
@@ -111,15 +110,61 @@ def fetch_and_slice_audio(audio_url):
         raise HTTPException(status_code=404, detail="Failed to fetch url")
 
 
-# @app.post('/transcript')
-# def transcriptWithModelz():
-#     if 'file' not in request.files:
-#         flash('No file part')
-#         return jsonify({'error': 'No file part'}), 400
-#     file = request.files['file']
-#     if file and allowed_file(file.filename):
-#         srt = transcript(file)
-#         return Response(srt, headers={
-#             'Content-Disposition': 'attachment; filename=audio.zip',
-#             'Content-Type': 'application/zip'
-#         })
+@app.get('/transcript')
+def transcript(audio_url: str, srt: bool):
+    print('Downloading:', audio_url)
+    print('Srt format', srt)
+    response = requests.get(audio_url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+
+    # Initialize the bytearray
+    content = bytearray()
+
+    for data in tqdm(response.iter_content(chunk_size=1024 * 1024), total=total_size // 1024 / 1024, unit='MB', unit_scale=True):
+        content.extend(data)
+
+    if response.status_code == 200:
+        print('Audio downloaded')
+        audio = AudioSegment.from_file(io.BytesIO(content))
+        # Slice into max 20-minute chunks
+        sliced_audios = slice_audio(audio, 20 * 60 * 1000)
+        results = []
+        for audio in sliced_audios:
+            slice_io = io.BytesIO()
+            audio.export(slice_io, format="mp3")
+            slice_io.seek(0)
+            with open('./audio.mp3', "wb") as f:
+                f.write(slice_io.read())
+            with open('./audio.mp3', "rb") as f:
+                transcript = openai.Audio.transcribe("whisper-1", f, api_key=OPENAI_API_KEY, response_format=format)
+                results.append(transcript if srt else transcript.text)
+        print('Request sent')
+        return results
+    else:
+        raise HTTPException(status_code=404, detail="Failed to fetch url")
+
+@app.post('/transcript')
+def transcript_file(file: UploadFile, srt: bool = False):
+    if file and allowed_file(file.filename):
+        audio = AudioSegment.from_file(file.file)
+        format = 'srt' if srt else 'text'
+        print('audio length:', len(audio))
+        # Slice into max 20-minute chunks
+        sliced_audios = slice_audio(audio, 20 * 60 * 1000)
+        results = []
+        for audio in sliced_audios:
+            slice_io = io.BytesIO()
+            audio.export(slice_io, format="mp3")
+            slice_io.seek(0)
+            filename = '/tmp/' + str(uuid.uuid4()) + '.mp3'
+            with open(filename, "wb") as f:
+                f.write(slice_io.read())
+            with open(filename, "rb") as f:
+                transcript = openai.Audio.transcribe("whisper-1", f, api_key=OPENAI_API_KEY, response_format=format)
+                print(transcript)
+                results.append(transcript if srt else transcript)
+            os.remove(filename)
+        print('Request sent')
+        return results
+    else:
+        raise HTTPException(status_code=404, detail="No file found")
