de302ba5ae6fc13f612e14f93c3f0ab2e0dfd8f4
stonega <xijieyin@gmail.com>
2023-05-03 16:06:36 +0800

    feat: support credit

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from datetime import datetime
 import os
-from fastapi import FastAPI, HTTPException, UploadFile
-from fastapi.responses import StreamingResponse
-from pydub import AudioSegment
-from tqdm import tqdm
 import io
 import requests
 import zipfile
 import openai
 import uuid
+import json
+from jose import jwe
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from datetime import datetime
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException, UploadFile
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+from pydub import AudioSegment
+from tqdm import tqdm
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
-load_dotenv()
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "_")
+from credit import get_user_credit, update_user_credit
 
+load_dotenv()
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "_")
 ALLOWED_EXTENSIONS = {'mp3', 'mp4', 'mpeg', 'mpga', 'm4a', 'wav', 'webm'}
 
 app = FastAPI()
@@ -23,6 +32,25 @@ origins = [
     "https://recos.vercel.app",
     "http://localhost:3000",
 ]
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    hkdf = HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=b"",
+        info="NextAuth.js Generated Encryption Key".encode(),
+    )
+    key = hkdf.derive(os.environ.get('JWT_SECRET').encode())
+    user = jwe.decrypt(token, key).decode()
+    return json.loads(user)
 
 app.add_middleware(
     CORSMiddleware,
@@ -111,10 +139,10 @@ def fetch_and_slice_audio(audio_url):
 
 
 @app.get('/transcript')
-def transcript(audio_url: str, srt: bool):
-    print('Downloading:', audio_url)
+def transcript(url: str, current_user: Annotated[User, Depends(get_current_user)], srt: bool = False ):
+    print('Downloading:', url)
     print('Srt format', srt)
-    response = requests.get(audio_url, stream=True)
+    response = requests.get(url, stream=True)
     total_size = int(response.headers.get('content-length', 0))
 
     # Initialize the bytearray
@@ -125,7 +153,12 @@ def transcript(audio_url: str, srt: bool):
 
     if response.status_code == 200:
         print('Audio downloaded')
+        credit = get_user_credit(current_user.id)
         audio = AudioSegment.from_file(io.BytesIO(content))
+        duration = round(audio.duration_seconds / 60)
+        if(duration > credit):
+            raise HTTPException(status_code=404, detail="Insufficient credit")
+        format = 'srt' if srt else 'text'
         # Slice into max 20-minute chunks
         sliced_audios = slice_audio(audio, 20 * 60 * 1000)
         results = []
@@ -133,20 +166,27 @@ def transcript(audio_url: str, srt: bool):
             slice_io = io.BytesIO()
             audio.export(slice_io, format="mp3")
             slice_io.seek(0)
-            with open('./audio.mp3', "wb") as f:
+            filename = '/tmp/' + str(uuid.uuid4()) + '.mp3'
+            with open(filename, "wb") as f:
                 f.write(slice_io.read())
-            with open('./audio.mp3', "rb") as f:
+            with open(filename, "rb") as f:
                 transcript = openai.Audio.transcribe("whisper-1", f, api_key=OPENAI_API_KEY, response_format=format)
                 results.append(transcript if srt else transcript.text)
+            os.remove(filename)
+        update_user_credit(current_user.id, -duration)
         print('Request sent')
         return results
     else:
         raise HTTPException(status_code=404, detail="Failed to fetch url")
 
 @app.post('/transcript')
-def transcript_file(file: UploadFile, srt: bool = False):
+def transcript_file(file: UploadFile,  current_user: Annotated[User, Depends(get_current_user)], srt: bool = False,):
     if file and allowed_file(file.filename):
+        credit = get_user_credit(current_user.id)
         audio = AudioSegment.from_file(file.file)
+        duration = round(audio.length_seconds / 60)
+        if(duration > credit):
+            raise HTTPException(status_code=404, detail="Insufficient credit")
         format = 'srt' if srt else 'text'
         print('audio length:', len(audio))
         # Slice into max 20-minute chunks
@@ -164,6 +204,7 @@ def transcript_file(file: UploadFile, srt: bool = False):
                 print(transcript)
                 results.append(transcript if srt else transcript)
             os.remove(filename)
+        update_user_credit(current_user.id, -duration)
         print('Request sent')
         return results
     else:
---
+++ credit.py
@@ -0,0 +1,23 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_user_credit(user_id: str):
+    print('connecting...')
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+    cursor = conn.cursor()
+    select_query = """SELECT * FROM "User" where id = %s;"""
+    cursor.execute(select_query, (user_id,))
+    user = cursor.fetchone()
+    return user[-1]
+
+def update_user_credit(user_id: str, credit: int):
+    print('connecting...')
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+    cursor = conn.cursor()
+    update_query = """UPDATE "User" SET credit = credit +  %s WHERE id = %s;"""
+    cursor.execute(update_query, (credit, user_id))
+    conn.commit()
+    return cursor.rowcount
