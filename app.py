a4208a545d76c4df94481d9c653dddaef60414a2
stonega <xijieyin@gmail.com>
2023-05-10 23:07:03 +0800

    fix: bug fix

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ def transcript(url: str, current_user: Annotated[User, Depends(get_current_user)
         # Slice into max 20-minute chunks
         sliced_audios = slice_audio(audio, 20 * 60 * 1000)
         with multiprocessing.Pool(processes=len(sliced_audios)) as pool:
-            results = pool.map(transcribe_audio, list(map(lambda audio:(audio, format), sliced_audios)))
+            results = pool.starmap(transcribe_audio, list(map(lambda audio:(audio, format), sliced_audios)))
         update_user_credit(current_user['sub'], -duration)
         print('Request sent')
         return results
