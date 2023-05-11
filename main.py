847aecdeb97353c24af060bf49f03f5c46ae3b63
stonega <xijieyin@gmail.com>
2023-05-11 23:09:12 +0800

    ci: remove workers

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import uvicorn
 
 if __name__ == "__main__":
     port=int(os.environ.get("PORT", "8000"))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, log_level="info", workers=10)
+    uvicorn.run("app:app", host="0.0.0.0", port=port, log_level="info")
