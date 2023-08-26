5c91850444e0c2ce65dcbe54be411e643b1af37b
stonega <xijieyin@gmail.com>
2023-08-26 17:46:49 +0800

    fix: minor fix

--- mongodb.py
+++ mongodb.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 from typing import List
 import motor.motor_asyncio
@@ -6,13 +7,13 @@ client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 db = client.subtitles
 
 async def do_insert(srt_items: List[dict], task_id: str):
-    srts = list(map(lambda s:  (task_id,
-                s['id'], s['start_time'], s['end_time'], s['text']), srt_items))
-    await db.insert_many(srts)
+    for srt in srt_items:
+        srt['task_id'] = task_id
+    await db.insert_many(srt_items)
 
 
 def save_subtitle_result_to_mongodb(srt_items, task_id: str):
-    new_loop = client.new_event_loop()
-    client.set_event_loop(new_loop)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     loop = client.get_io_loop()
     loop.run_until_complete(do_insert(srt_items=srt_items, task_id=task_id))
