4f4692638f9a6a1a0063e2bb884b806aabf6af7f
stonega <xijieyin@gmail.com>
2023-08-26 17:07:34 +0800

    fix: change async function to sync

--- mongodb.py
+++ mongodb.py
@@ -5,8 +5,12 @@ import motor.motor_asyncio
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 db = client.subtitles
 
-async def save_subtitle_result_to_mongodb(srt_items, task_id: str):
-    print('save result...')
+async def do_insert(srt_items: List[dict], task_id: str):
     srts = list(map(lambda s:  (task_id,
                 s['id'], s['start_time'], s['end_time'], s['text']), srt_items))
     await db.insert_many(srts)
+
+
+def save_subtitle_result_to_mongodb(srt_items, task_id: str):
+    loop = client.get_io_loop()
+    loop.run_until_complete(do_insert(srt_items=srt_items, task_id=task_id))
--- worker.py
+++ worker.py
@@ -158,7 +158,7 @@ def transcript_file_task_add(file: bytes, user, srt: bool = False, prompt: str =
     srts = parse_srt(merge_multiple_srt_strings(*results))  # type: ignore
     # Save subtitles
     save_subtitle_result(srts, transcript_file_task_add.request.id)
-    # await save_subtitle_result_to_mongodb(srts, transcript_file_task_add.request.id)
+    save_subtitle_result_to_mongodb(srts, transcript_file_task_add.request.id)
     print('Request sent')
     return
 
