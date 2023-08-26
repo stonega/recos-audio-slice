83cc001c05333cac0cf2c1c130df7dbd4878302b
stonega <xijieyin@gmail.com>
2023-08-26 17:55:09 +0800

    fix: minor fix

--- mongodb.py
+++ mongodb.py
@@ -9,7 +9,7 @@ db = client.subtitles
 async def do_insert(srt_items: List[dict], task_id: str):
     for srt in srt_items:
         srt['task_id'] = task_id
-    await db.insert_many(srt_items)
+    await db.subtitle.insert_many(srt_items)
 
 
 def save_subtitle_result_to_mongodb(srt_items, task_id: str):
